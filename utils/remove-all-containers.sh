28fcceb384c3672a81ac10d6f1438213f6484d67
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-03-18 06:47:23 +0300

    Add a script that removes all containers

---
+++ utils/remove-all-containers.sh
@@ -0,0 +1,15 @@
+read -p "All containers will be stopped and removed. Are you sure? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    docker stop bginetaccounting
+    docker stop bginetaccess
+    docker stop bgbilling
+    docker stop mysql
+    docker stop activemq
+    docker rm bginetaccounting
+    docker rm bginetaccess
+    docker rm bgbilling
+    docker rm mysql
+    docker rm activemq
+fi
