5054a171aa0aba85b8acabd3ebb05f8326d031c8
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-03-21 14:19:57 +0300

    In order for DHCP to work, bginetaccess container must run using the docker run --net host option

--- bgbilling/run.sh
+++ bgbilling/run.sh
@@ -1,14 +1,14 @@
 docker run --name mysql --detach \
   --env MYSQL_ALLOW_EMPTY_PASSWORD=yes \
   --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
-  --publish-all \
+  --publish 3306:3306 \
   mysql:5.5.48
 
 docker run --rm --link mysql:mysql martin/wait -p 3306
 
 docker run --name activemq --detach \
   --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
-  --publish-all \
+  --publish 61616:61616 \
   rmohr/activemq:5.13.1
 
 docker run --rm --link activemq:activemq martin/wait -p 61616
--- bginetaccess/run.sh
+++ bginetaccess/run.sh
@@ -1,13 +1,14 @@
+docker run --rm --link mysql:mysql martin/wait -p 3306
+docker run --rm --link activemq:activemq martin/wait -p 61616
 docker run --rm --link bgbilling:bgbilling martin/wait -p 8080 -t 300
 
 docker run --name bginetaccess --detach \
+  --net host \
   --env APP_NAME=BGInetAccess1 \
   --env APP_ID=31415 \
   --env MODULE_ID=1 \
   --env ROOT_DEVICE_ID=1 \
   --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
-  --link mysql:mysql \
-  --link activemq:activemq \
+  --publish 67:67/udp \
   --publish 1812:1812/udp \
-  --publish 67:10067/udp \
   alexanderfefelov/docker-bginetaccess
