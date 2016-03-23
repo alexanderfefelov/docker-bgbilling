93ffffb59b2fe0a7688c340569ab2ac1062650c4
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-03-23 11:22:16 +0300

    Add Docker garbage collection

---
+++ utils/docker-gc.sh
@@ -0,0 +1,6 @@
+read -p "All garbage will be collected. Are you sure? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  docker run --rm --volume /var/run/docker.sock:/var/run/docker.sock --volume /etc:/etc spotify/docker-gc
+fi
