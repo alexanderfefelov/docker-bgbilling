228de809f5bf1c09bd2a584234c723d5631bcaba
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-03-23 05:14:12 +0300

    Work on database population

--- step-by-step/030-populate-database.sh
+++ step-by-step/030-populate-database.sh
@@ -9,6 +9,6 @@ SOAP_BASE_URL=http://localhost:8080/bgbilling/executer
 while read -r line
 do
   IFS=' ' read -a query <<< $line
-  echo ${query[0]} '->' ${query[1]}
+  echo -n ${query[0]} '->' ${query[1]}...
   curl --header "Content-Type: text/xml" --request POST --silent --output /dev/null --write-out "%{http_code}\n" --data-binary @$DATA_DIR/soap/${query[0]} $SOAP_BASE_URL/${query[1]}
 done < $DATA_DIR/soap/queries
