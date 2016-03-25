afe1a64d054558bf3669a13cbe168fde72fd754f
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-03-25 06:37:07 +0300

    Refactoring

--- step-by-step/015-populate-database.sh
+++
@@ -1,6 +0,0 @@
-DATA_DIR=015-populate-database
-
-for file in $DATA_DIR/*.sql; do
-  echo $file
-  mysql --host=127.0.0.1 bgbilling < $file
-done
--- step-by-step/030-populate-database.sh
+++ step-by-step/030-populate-database.sh
@@ -1,14 +1,19 @@
 DATA_DIR=030-populate-database
 
-for file in $DATA_DIR/sql/*.sql; do
-  echo $file
-  mysql --host=127.0.0.1 bgbilling < $file
+for dir in $DATA_DIR/010-sql/*; do
+  echo $dir
+  for file in $dir/*.sql; do
+    echo $file
+    mysql --host=127.0.0.1 bgbilling < $file
+  done
 done
 
 SOAP_BASE_URL=http://localhost:8080/bgbilling/executer
 while read -r line
 do
-  IFS=' ' read -a query <<< $line
-  echo -n ${query[0]} '->' ${query[1]}...
-  curl --header "Content-Type: text/xml" --request POST --silent --output /dev/null --write-out "%{http_code}\n" --data-binary @$DATA_DIR/soap/${query[0]} $SOAP_BASE_URL/${query[1]}
-done < $DATA_DIR/soap/queries
+  if [ -n "$line" ]; then
+    IFS=' ' read -a query <<< $line
+    echo -n ${query[0]}...
+    curl --header "Content-Type: text/xml" --request POST --silent --output /dev/null --write-out "%{http_code}\n" --data-binary @$DATA_DIR/020-soap/${query[0]} $SOAP_BASE_URL/${query[1]}
+  fi
+done < $DATA_DIR/020-soap/queries
