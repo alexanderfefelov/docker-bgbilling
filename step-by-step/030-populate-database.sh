ca12334678b340da76b8101008c2667ef8e2afbc
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-03-29 16:51:32 +0300

    Work on database population

--- step-by-step/030-populate-database.sh
+++ step-by-step/030-populate-database.sh
@@ -1,4 +1,6 @@
 DATA_DIR=030-populate-database
+SOAP_BASE_URL=http://localhost:8080/bgbilling/executer
+HTTP_BASE_URL=http://localhost:8080/bgbilling/executer
 
 for dir in $DATA_DIR/010-sql/*; do
   echo $dir
@@ -8,7 +10,6 @@ for dir in $DATA_DIR/010-sql/*; do
   done
 done
 
-SOAP_BASE_URL=http://localhost:8080/bgbilling/executer
 while read -r line
 do
   if [ -n "$line" ]; then
@@ -18,7 +19,6 @@ do
   fi
 done < $DATA_DIR/020-soap/queries
 
-HTTP_BASE_URL=http://localhost:8080/bgbilling/executer
 for dir in $DATA_DIR/030-http/*; do
   for file in $dir/*.http; do
     echo $file
@@ -31,3 +31,12 @@ for dir in $DATA_DIR/030-http/*; do
     done < $file
   done
 done
+
+while read -r line
+do
+  if [ -n "$line" ]; then
+    IFS=' ' read -a query <<< $line
+    echo -n ${query[0]}...
+    curl --header "Content-Type: text/xml" --request POST --silent --output /dev/null --write-out "%{http_code}\n" --data-binary @$DATA_DIR/040-soap/${query[0]} $SOAP_BASE_URL/${query[1]}
+  fi
+done < $DATA_DIR/040-soap/queries
