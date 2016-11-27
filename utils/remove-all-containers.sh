891cda371f27dfc325fcf5bf60b0645dd7936a5c
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-11-28 02:53:18 +0300

    Minor fix

--- utils/remove-all-containers.sh
+++ utils/remove-all-containers.sh
@@ -4,14 +4,9 @@ read -p "All containers will be stopped and removed. Are you sure? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-    docker stop bginetaccounting
-    docker stop bginetaccess
-    docker stop bgbilling
-    docker stop mysql
-    docker stop activemq
-    docker rm bginetaccounting
-    docker rm bginetaccess
-    docker rm bgbilling
-    docker rm mysql
-    docker rm activemq
+    docker rm --force --volumes bginetaccounting
+    docker rm --force --volumes bginetaccess
+    docker rm --force --volumes bgbilling
+    docker rm --force --volumes mysql
+    docker rm --force --volumes activemq
 fi
