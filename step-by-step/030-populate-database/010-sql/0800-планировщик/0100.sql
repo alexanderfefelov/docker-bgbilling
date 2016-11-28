dd5e1135d5af009804e1dad62c41c4638a2e52dc
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-11-28 12:32:05 +0300

    Work on database population

---
+++ step-by-step/030-populate-database/010-sql/0800-планировщик/0100.sql
@@ -0,0 +1,3 @@
+insert into bgbilling.scheduled_tasks(id, mm, dm, dw, hh, min, `prior`, date1, date2, status, class_id, class, module_id, comment, params) values
+  (1, 0, 0, 0, 0, 0, 1, null, null, 1, -1, 'ru.bitel.bgbilling.modules.npay.server.Calculator', '2', '', 'mid=2'),
+  (2, 0, 0, 0, 0, 0, 1, null, null, 1, -1, 'ru.bitel.bgbilling.modules.rscm.server.Calculator', '3', '', 'mid=3');
