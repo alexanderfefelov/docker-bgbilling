db78311868e7f23b480b5331e0b81fbbbcc41982
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-12-06 11:46:20 +0300

    Work on database population

---
+++ step-by-step/030-populate-database/010-sql/0650-справочник-групп-договоров/0100.sql
@@ -0,0 +1,5 @@
+-- Справочники -> Другие -> Договоры - группы
+insert into contract_group(id, title, enable, editable, comment) values
+  (0, 'Сотрудник', 1, 1, ''),
+  (1, 'Физ. лицо', 1, 1, ''),
+  (2, 'Юр. лицо', 1, 1, '');
