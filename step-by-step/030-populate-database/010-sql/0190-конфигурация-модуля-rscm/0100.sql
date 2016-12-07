449692901b8005a93104ec7c960b16144f9d3c24
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-12-07 13:02:27 +0300

    Work on database population

--- step-by-step/030-populate-database/010-sql/0190-конфигурация-модуля-rscm/0100.sql
+++ step-by-step/030-populate-database/010-sql/0190-конфигурация-модуля-rscm/0100.sql
@@ -1,4 +1,6 @@
 -- Модули -> Разовые услуги -> Конфигурация модуля
 insert into module_config(mid, uid, dt, title, active, config)
   values(3, 1, now(), 'Default', 1, '
+# Начисление денег сразу по добавлению услуги в договор
+hot.calc=1
 ');
