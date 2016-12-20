de1069522eab3e30f8c624677d5da1334d7f427c
Alexander Fefelov <alexanderfefelov@yandex.ru>
2016-12-20 14:26:58 +0300

    Fix UDP listener

--- bginetaccounting/run.sh
+++ bginetaccounting/run.sh
@@ -5,13 +5,12 @@ docker run --rm --link activemq:activemq martin/wait -p 61616
 docker run --rm --link bgbilling:bgbilling martin/wait -p 8080 -t 300
 
 docker run --name bginetaccounting --detach \
+  --net host \
   --env APP_NAME=BGInetAccounting1 \
   --env APP_ID=314159 \
   --env MODULE_ID=1 \
   --env ROOT_DEVICE_ID=1 \
   --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
-  --link mysql:mysql \
-  --link activemq:activemq \
   --publish 1813:1813/udp \
   --publish 2001:2001/udp \
   alexanderfefelov/docker-bginetaccounting
