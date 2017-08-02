49d299c1e3223eef2b67b715857b57d18ca53f2c
Alexander Fefelov <alexanderfefelov@yandex.ru>
2017-08-02 11:10:12 +0300

    Update all the stuff

--- bgbilling/run.sh
+++ bgbilling/run.sh
@@ -4,7 +4,7 @@ docker run --name mysql --detach \
   --env MYSQL_ALLOW_EMPTY_PASSWORD=yes \
   --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
   --publish 3306:3306 \
-  mysql:5.5.48
+  mysql:5.5.57
 
 docker run --rm --link mysql:mysql martin/wait -p 3306
 
@@ -12,7 +12,7 @@ docker run --name activemq --detach \
   --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
   --publish 61616:61616 \
   --publish 8161:8161 \
-  rmohr/activemq:5.13.1
+  rmohr/activemq:5.14.3
 
 docker run --rm --link activemq:activemq martin/wait -p 61616
 
