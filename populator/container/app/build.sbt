5d28b85521e05be34e361dff20d909eaa3cc3478
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-02-08 16:55:19 +0300

    Work on populator

--- populator/container/app/build.sbt
+++ populator/container/app/build.sbt
@@ -3,5 +3,26 @@ name := "populator"
 scalaVersion := "2.11.12"
 
 libraryDependencies ++= Seq(
-  "com.github.alexanderfefelov" %% "bgbilling-api-db-oss" % "0.1.0-SNAPSHOT"
+  "joda-time" % "joda-time" % "2.9.9",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-bill" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-bonus" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-card" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-dispatch" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-inet" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-kernel" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-moneta" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-mps" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-oss" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-qiwi" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-rscm" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-db-subscription" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-soap-bill" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-soap-card" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-soap-inet" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-soap-kernel" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-soap-moneta" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-soap-oss" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-soap-qiwi" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-soap-rscm" % "0.1.0-SNAPSHOT",
+  "com.github.alexanderfefelov" %% "bgbilling-api-soap-subscription" % "0.1.0-SNAPSHOT"
 )
--- populator/container/app/src/main/scala/Main.scala
+++ populator/container/app/src/main/scala/Main.scala
@@ -1,7 +1,75 @@
 import com.github.alexanderfefelov.bgbilling.api.db.repository._
+import com.github.alexanderfefelov.bgbilling.api.db.util.Db
+import com.github.alexanderfefelov.bgbilling.api.soap.util.ApiSoapConfig
+import org.joda.time.DateTime
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scalaxb.DispatchHttpClientsAsync
 
 object Main extends App {
 
-  println("Hello, world!")
+  Db.init()
+
+  addresses()
+  contractPaymentTypes()
+  modulesAndServices()
+  inetDeviceTypes()
+
+  println("Finished")
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Справочники -> Адреса
+  //
+  private def addresses() = {
+    val countryId = AddressCountry.create("РФ").id
+    val cityId = AddressCity.create(countryId, "г. Звенигород").id
+    val streetId = AddressStreet.create(cityid = cityId, title = "ул. Мира", pIndex = "143180").id
+    val houseId = AddressHouse.create(streetid = streetId, house = 1, frac = Some("Б"), amount = 128, comment = None, areaid = 0, quarterid = 0, boxIndex = None, dt = None, podDiapazon = "", pod = "").id
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Справочники -> Другие -> Типы платежей
+  //
+  private def contractPaymentTypes() = {
+    ContractPaymentTypes.create(title = "Наличные", up = 0, `type` = 0, flag = 0)
+    ContractPaymentTypes.create(title = "Банковская карта (офлайн)", up = 0, `type` = 0, flag = 0)
+
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Модули -> Редактор модулей и услуг
+  //
+  private def modulesAndServices() = {
+    import com.github.alexanderfefelov.bgbilling.api.db.repository.{Service => DbService}
+    import com.github.alexanderfefelov.bgbilling.api.soap.kernel._
+    import com.github.alexanderfefelov.bgbilling.api.soap.scalaxb._
+
+    class ModuleServiceCake extends ModuleServiceBindings with Soap11ClientsWithAuthHeaderAsync with DispatchHttpClientsAsync with ApiSoapConfig {
+      override def baseAddress = new java.net.URI(soapServiceBaseAddress("ModuleService"))
+    }
+    val moduleServiceCake = new ModuleServiceCake
+    val moduleService = moduleServiceCake.service
+
+    val inetModuleIdFuture = moduleService.moduleAdd(Some("inet"), Some("Интернет"))
+    val inetModuleId = Await.result(inetModuleIdFuture, 2.minutes)
+    DbService.create("Доступ в интернет", mid = inetModuleId, parentid = 0, datefrom = None, dateto = None, comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 30000)
+
+    val npayModuleIdFuture = moduleService.moduleAdd(Some("npay"), Some("Периодические услуги"))
+    val npayModuleId = Await.result(npayModuleIdFuture, 60.seconds)
+    DbService.create("Абонентская плата", mid = npayModuleId, parentid = 0, datefrom = None, dateto = None, comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 10000)
+
+    val rscmModuleIdFuture = moduleService.moduleAdd(Some("rscm"), Some("Разовые услуги"))
+    val rscmModuleId = Await.result(rscmModuleIdFuture, 60.seconds)
+    DbService.create("Подключение", mid = rscmModuleId, parentid = 0, datefrom = None, dateto = None, comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 10000)
+
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Модули -> Интернет -> Устройства и ресурсы -> Типы устройств
+  //
+  private def inetDeviceTypes() = {
+    InetDeviceType1.create(title = "Access + Accounting", configid = 0, config = "", protocolhandlerclass = Some("com.github.alexanderfefelov.bgbilling.device.murmuring.MurmuringProtocolHandler"), sahandlerclass = Some("com.github.alexanderfefelov.bgbilling.device.murmuring.MurmuringServiceActivator"), devicemanagerclass = None, uniqueinterfaces = 0, scriptid = 0, sascript = None, eventscript = None, comment = "", source = None, deviceentityspecid = 0)
+  }
 
 }
