f5ab8aadcd4f097e50a3b252951004797d340908
root <alexanderfefelov@yandex.ru>
2018-02-13 15:30:29 +0300

    Work on populator

--- bginetaccess/run.sh
+++ bginetaccess/run.sh
@@ -9,7 +9,7 @@ docker run --name bginetaccess --detach \
   --env APP_NAME=BGInetAccess1 \
   --env APP_ID=31415 \
   --env MODULE_ID=1 \
-  --env ROOT_DEVICE_ID=1 \
+  --env ROOT_DEVICE_ID=2 \
   --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
   --publish 67:67/udp \
   --publish 1812:1812/udp \
--- bginetaccounting/run.sh
+++ bginetaccounting/run.sh
@@ -9,7 +9,7 @@ docker run --name bginetaccounting --detach \
   --env APP_NAME=BGInetAccounting1 \
   --env APP_ID=314159 \
   --env MODULE_ID=1 \
-  --env ROOT_DEVICE_ID=1 \
+  --env ROOT_DEVICE_ID=2 \
   --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
   --publish 1813:1813/udp \
   --publish 2001:2001/udp \
