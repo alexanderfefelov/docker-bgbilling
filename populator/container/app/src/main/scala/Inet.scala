cb2f54addb9e49dc24f64b62f8064cb0c6884494
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-03-07 10:53:13 +0300

    Work on populator

--- populator/container/app/src/main/scala/Inet.scala
+++ populator/container/app/src/main/scala/Inet.scala
@@ -128,9 +128,23 @@ object Inet {
   // Модули -> Интернет -> Справочники -> Типы сервисов
   //
   def servTypes() = {
-    InetServType1.create(title = "Динамический серый адрес", config = Some("ip.resource.categoryId=1"), parenttypeids = "", sessioninitiationtype = 0, sessioncountlimit = 1, sessioncountlimitlock = 1, addresstype = 4, addressallinterface = 1, traffictypelinkid = 0, needlogin = 0, needdevice = 1, needinterface = 1, personalinterface = 1, needvlan = 1, needidentifier = 0, needmacaddress = 0, needcontractobject = 0, needrestriction = 0, personalvlan = 1)
-    InetServType1.create(title = "Динамический белый адрес", config = Some("ip.resource.categoryId=2"), parenttypeids = "", sessioninitiationtype = 0, sessioncountlimit = 1, sessioncountlimitlock = 1, addresstype = 4, addressallinterface = 1, traffictypelinkid = 0, needlogin = 0, needdevice = 1, needinterface = 1, personalinterface = 1, needvlan = 1, needidentifier = 0, needmacaddress = 0, needcontractobject = 0, needrestriction = 0, personalvlan = 1)
-    InetServType1.create(title = "Статический белый адрес", config = Some(""), parenttypeids = "", sessioninitiationtype = 0, sessioncountlimit = 1, sessioncountlimitlock = 1, addresstype = 4, addressallinterface = 1, traffictypelinkid = 0, needlogin = 0, needdevice = 1, needinterface = 1, personalinterface = 1, needvlan = 1, needidentifier = 0, needmacaddress = 0, needcontractobject = 0, needrestriction = 0, personalvlan = 1)
+    var cfg =
+      """
+        |ip.resource.categoryId=1
+        |title.pattern=Динамический серый адрес, VLAN ${vlan}
+      """.stripMargin
+    InetServType1.create(title = "Динамический серый адрес", config = Some(cfg), parenttypeids = "", sessioninitiationtype = 0, sessioncountlimit = 1, sessioncountlimitlock = 1, addresstype = 4, addressallinterface = 1, traffictypelinkid = 0, needlogin = 0, needdevice = 1, needinterface = 1, personalinterface = 1, needvlan = 1, needidentifier = 0, needmacaddress = 0, needcontractobject = 0, needrestriction = 0, personalvlan = 1)
+    cfg =
+      """
+        |ip.resource.categoryId=2
+        |title.pattern=Динамический белый адрес, VLAN ${vlan}
+        |      """.stripMargin
+    InetServType1.create(title = "Динамический белый адрес", config = Some(cfg), parenttypeids = "", sessioninitiationtype = 0, sessioncountlimit = 1, sessioncountlimitlock = 1, addresstype = 4, addressallinterface = 1, traffictypelinkid = 0, needlogin = 0, needdevice = 1, needinterface = 1, personalinterface = 1, needvlan = 1, needidentifier = 0, needmacaddress = 0, needcontractobject = 0, needrestriction = 0, personalvlan = 1)
+    cfg =
+      """
+        |title.pattern=Статический белый адрес ${addressIp}, VLAN ${vlan}
+      """.stripMargin
+    InetServType1.create(title = "Статический белый адрес", config = Some(cfg), parenttypeids = "", sessioninitiationtype = 0, sessioncountlimit = 1, sessioncountlimitlock = 1, addresstype = 3, addressallinterface = 1, traffictypelinkid = 0, needlogin = 0, needdevice = 1, needinterface = 1, personalinterface = 1, needvlan = 1, needidentifier = 0, needmacaddress = 0, needcontractobject = 0, needrestriction = 0, personalvlan = 1)
     InetServTypeDeviceGroupLink1.create(inetservid = 1, devicegroupid = 3)
     InetServTypeDeviceGroupLink1.create(inetservid = 2, devicegroupid = 3)
     InetServTypeDeviceGroupLink1.create(inetservid = 2, devicegroupid = 3)
