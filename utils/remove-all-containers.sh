9791b473ba0cede6ce7d7bb3d3458e121a3686f9
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-05-01 05:17:12 +0300

    Use named volumes

--- utils/remove-all-containers.sh
+++ utils/remove-all-containers.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-read -p "All containers will be stopped and removed. Are you sure? " -n 1 -r
+read -p "All containers and volumes will be removed. Are you sure? " -n 1 -r
 echo
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
     docker rm --force --volumes bginetaccounting
+    docker volume rm bginetaccounting
+
     docker rm --force --volumes bginetaccess
+    docker volume rm bginetaccess
+
     docker rm --force --volumes bgbilling
+    docker volume rm bgbilling
+
     docker rm --force --volumes mysql
     docker rm --force --volumes activemq
 fi
