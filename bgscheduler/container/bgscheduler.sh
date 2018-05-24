026ec2b65c77bc11c8713c7a187a49fc7f2f179a
root <alexanderfefelov@yandex.ru>
2018-05-24 16:31:16 +0300

    Используй отдельный контейнер для планировщика

--- bgscheduler/container/bgscheduler.sh
+++ bgscheduler/container/bgscheduler.sh
@@ -2,4 +2,23 @@
 
 export BGBILLING_HOME=/bgbilling
 
+# Если в каталоге install существуют zip-файлы, считаем их модулями (плагинами),
+# извлекаем из них server.jar, переносим server.jar в lib/app, и удаляем zip-файлы
+#
+MODULES=$BGBILLING_HOME/install/*.zip
+for m in $MODULES
+do
+  if [ -f $m ]
+  then
+    echo Installing $m
+    filename=`basename $m .zip`
+    IFS=_ read name version <<EOF
+$filename
+EOF
+    unzip -p $m server.zip | funzip > $name.jar
+    mv --force $name.jar $BGBILLING_HOME/lib/app
+    rm -f $m
+  fi
+done
+
 service bgscheduler start
