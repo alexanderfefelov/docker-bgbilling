b583a7909d8f4bf5ba58e472773864bded5c0af5
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-05-25 10:06:46 +0300

    Добавь в планировщик задание "Валидатор"

---
+++ populator/container/app/src/main/scala/modules/Kernel.scala
@@ -0,0 +1,20 @@
+package modules
+
+import com.github.alexanderfefelov.bgbilling.api.db.repository.{ScheduledTasks, Service => DbService}
+import com.github.alexanderfefelov.bgbilling.api.soap.kernel.ModuleService
+import org.joda.time.DateTime
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+object Kernel {
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Сервис -> Администрирование -> Планировщик заданий
+  //
+  def scheduledTasks() = {
+    ScheduledTasks.create(mm = 0, dm = 0, dw = 0, hh = 16, min = 1048576, prior = 1, date1 = None, date2 = None, status = 1, classId = -1,
+      `class` = "ru.bitel.bgbilling.kernel.task.server.Validator", moduleId = "0", comment = "Проверка базы данных биллинга на корректность", params = "email=admin@inter.net")
+  }
+
+}
