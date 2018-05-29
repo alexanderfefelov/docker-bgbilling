3a9f280bcadfe688cfd76cdd9a2c5051e8299a22
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-05-29 06:26:33 +0300

    Work on populator

--- populator/container/app/src/main/scala/Main.scala
+++ populator/container/app/src/main/scala/Main.scala
@@ -15,18 +15,19 @@ import scalaxb.DispatchHttpClientsAsync
 object Main extends App {
 
   implicit val charset: Charset = Charset.forName("UTF-8")
+  val now = DateTime.now()
 
   Db.init()
 
-  dynamicCodeRecompile()
+  Kernel.dynamicCodeRecompile()
   moduleConfigs()
-  bgsGroups()
-  addresses()
-  contractPaymentTypes()
-  contractParametersPrefs()
-  contractParameterGroups()
-  сontractParameterType7Values()
-  entitySpecs()
+  Kernel.bgsGroups()
+  Kernel.addresses()
+  Kernel.contractPaymentTypes()
+  Kernel.contractParametersPrefs()
+  Kernel.contractParameterGroups()
+  Kernel.сontractParameterType7Values()
+  Kernel.entitySpecs()
   modulesAndServices()
   plugins()
   scheduledTasks()
@@ -42,23 +43,6 @@ object Main extends App {
   Inet.deviceReload()
 
   println("Finished. Press Ctrl+C")
-
-  //--------------------------------------------------------------------------------------------------------------------
-  // Сервис -> Автоматизация -> Управление динамическим кодом -> Скомпилировать всё
-  //
-  private def dynamicCodeRecompile(): Unit = {
-    import com.github.alexanderfefelov.bgbilling.api.soap.kernel._
-    import com.github.alexanderfefelov.bgbilling.api.soap.scalaxb._
-
-    class DynamicCodeServiceCake extends DynamicCodeServiceBindings with Soap11ClientsWithAuthHeaderAsync with ConfigurableDispatchHttpClientsAsync with ApiSoapConfig {
-      override def baseAddress = new java.net.URI(soapServiceBaseAddress("dynamic-code-service"))
-    }
-    val dynamicCodeServiceCake = new DynamicCodeServiceCake
-    val dynamicCode = dynamicCodeServiceCake.service
-
-    val responseFuture = dynamicCode.recompileAll()
-    Await.result(responseFuture, 5.minutes)
-  }
 
   //--------------------------------------------------------------------------------------------------------------------
   // Сервис -> Настройка -> Конфигурация
@@ -82,106 +66,10 @@ object Main extends App {
       "bill"
     )
     for (i <- modules.indices) {
-      ModuleConfig.create(mid = Some(i), dt = DateTime.now(), title = "Default", active = 1, uid = Some(1),
+      ModuleConfig.create(mid = Some(i), dt = now, title = "Default", active = 1, uid = Some(1),
         config = Some(File(s"bgbilling/${modules(i)}.conf").contentAsString)
       )
     }
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  // Сервис -> Администрирование -> Пользователи и права -> Группы
-  //
-  private def bgsGroups(): Unit = {
-    BgsGroup.create(title = "Системные администраторы", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
-    BgsGroup.create(title = "Техническая поддержка", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
-    BgsGroup.create(title = "Строительный отдел", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
-    BgsGroup.create(title = "Абонентский отдел", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
-    BgsGroup.create(title = "Ресепшен", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
-    BgsGroup.create(title = "Бухгалтерия", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
-    BgsGroup.create(title = "Ночной колл-центр", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  // Справочники -> Адреса
-  //
-  private def addresses(): Unit = {
-    val countryId = AddressCountry.create(title = "РФ").id
-    val cityId = AddressCity.create(countryId = countryId, title = "г. Звенигород").id
-    val streetId = AddressStreet.create(cityid = cityId, title = "ул. Мира", pIndex = "143180").id
-    val houseId = AddressHouse.create(streetid = streetId, house = 1, frac = Some("Б"), amount = 128, comment = None, areaid = 0, quarterid = 0, boxIndex = None, dt = None, podDiapazon = "", pod = "").id
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  // Справочники -> Другие -> Типы платежей
-  //
-  private def contractPaymentTypes(): Unit = {
-    ContractPaymentTypes.create(title = "Наличные", up = 0, `type` = 0, flag = 0)
-    ContractPaymentTypes.create(title = "Банковская карта (офлайн)", up = 0, `type` = 0, flag = 0)
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  // Справочники -> Другие -> Договоры - параметры
-  //
-  private def contractParametersPrefs(): Unit = {
-    // Физ. лица
-    //
-    ContractParametersPref.create(pt = 2, title = "Адрес подключения", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 1, title = "Адрес регистрации", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 9, title = "Телефон моб.", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 9, title = "Телефон дом.", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 3, title = "Email", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 1, title = "Данные паспорта", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 6, title = "Дата рождения", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 1, title = "Место рождения", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    // Юр. лица
-    //
-    ContractParametersPref.create(pt = 1, title = "ИНН", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 1, title = "Телефон(ы)", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 1, title = "Адрес юридический", sort = 1, script = "", flags = 1, lm = DateTime.now())
-    ContractParametersPref.create(pt = 7, title = "Организационно-правовая форма", sort = 1, script = "", flags = 1, lm = DateTime.now())
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  // Справочники -> Другие -> Договоры - группы параметров
-  //
-  private def contractParameterGroups(): Unit = {
-    ContractParameterGroupName.create("Физ. лицо")
-    ContractParameterGroup.create(gid = 1, pid = 1)
-    ContractParameterGroup.create(gid = 1, pid = 2)
-    ContractParameterGroup.create(gid = 1, pid = 3)
-    ContractParameterGroup.create(gid = 1, pid = 4)
-    ContractParameterGroup.create(gid = 1, pid = 5)
-    ContractParameterGroup.create(gid = 1, pid = 6)
-    ContractParameterGroup.create(gid = 1, pid = 7)
-    ContractParameterGroup.create(gid = 1, pid = 8)
-    ContractParameterGroupName.create("Юр. лицо")
-    ContractParameterGroup.create(gid = 2, pid = 1)
-    ContractParameterGroup.create(gid = 2, pid = 9)
-    ContractParameterGroup.create(gid = 2, pid = 12)
-    ContractParameterGroup.create(gid = 2, pid = 10)
-    ContractParameterGroup.create(gid = 2, pid = 11)
-    ContractParameterGroup.create(gid = 2, pid = 5)
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  // Справочники -> Другие -> Договоры - значения списков -> Значения списков
-  //
-  private def сontractParameterType7Values(): Unit = {
-    ContractParameterType7Values.create(pid = 12, title = "ООО")
-    ContractParameterType7Values.create(pid = 12, title = "ЗАО")
-    ContractParameterType7Values.create(pid = 12, title = "ПАО")
-  }
-
-  //--------------------------------------------------------------------------------------------------------------------
-  // Справочники -> Атрибуты -> Атрибуты
-  // Справочники -> Атрибуты -> Сущности
-  //
-  private def entitySpecs(): Unit = {
-    EntitySpecAttr.create(title = "Адрес", `type` = 8, comment = "")
-    EntitySpecAttr.create(title = "S/N", `type` = 1, comment = "")
-    EntitySpec.create(title = "Коммутатор", entityspectypeid = 0, comment = "", hidden = 0, entitytitlemacros = "")
-    EntitySpecAttrLink.create(entityspecid = 1, entityspecattrid = 1, pos = 0)
-    EntitySpecAttrLink.create(entityspecid = 1, entityspecattrid = 2, pos = 0)
   }
 
   //--------------------------------------------------------------------------------------------------------------------
--- populator/container/app/src/main/scala/modules/Kernel.scala
+++ populator/container/app/src/main/scala/modules/Kernel.scala
@@ -1,13 +1,16 @@
 package modules
 
-import com.github.alexanderfefelov.bgbilling.api.db.repository.{ScheduledTasks, Service => DbService}
-import com.github.alexanderfefelov.bgbilling.api.soap.kernel.ModuleService
+import com.github.alexanderfefelov.bgbilling.api.db.repository._
+import com.github.alexanderfefelov.bgbilling.api.soap.util.ApiSoapConfig
 import org.joda.time.DateTime
 
 import scala.concurrent.Await
 import scala.concurrent.duration._
 
+
 object Kernel {
+
+  val now = DateTime.now()
 
   //--------------------------------------------------------------------------------------------------------------------
   // Сервис -> Администрирование -> Планировщик заданий
@@ -15,6 +18,126 @@ object Kernel {
   def scheduledTasks() = {
     ScheduledTasks.create(mm = 0, dm = 0, dw = 0, hh = 16, min = 1048576, prior = 1, date1 = None, date2 = None, status = 1, classId = -1,
       `class` = "ru.bitel.bgbilling.kernel.task.server.Validator", moduleId = "0", comment = "Проверка базы данных биллинга на корректность", params = "email=admin@inter.net")
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Сервис -> Автоматизация -> Управление динамическим кодом -> Скомпилировать всё
+  //
+  def dynamicCodeRecompile(): Unit = {
+    import com.github.alexanderfefelov.bgbilling.api.soap.kernel._
+    import com.github.alexanderfefelov.bgbilling.api.soap.scalaxb._
+
+    class DynamicCodeServiceCake extends DynamicCodeServiceBindings with Soap11ClientsWithAuthHeaderAsync with ConfigurableDispatchHttpClientsAsync with ApiSoapConfig {
+      override def baseAddress = new java.net.URI(soapServiceBaseAddress("dynamic-code-service"))
+    }
+    val dynamicCodeServiceCake = new DynamicCodeServiceCake
+    val dynamicCode = dynamicCodeServiceCake.service
+
+    val responseFuture = dynamicCode.recompileAll()
+    Await.result(responseFuture, 5.minutes)
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Сервис -> Администрирование -> Пользователи и права -> Группы
+  //
+  def bgsGroups(): Unit = {
+    BgsGroup.create(title = "Системные администраторы", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
+    BgsGroup.create(title = "Техническая поддержка", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
+    BgsGroup.create(title = "Строительный отдел", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
+    BgsGroup.create(title = "Абонентский отдел", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
+    BgsGroup.create(title = "Ресепшен", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
+    BgsGroup.create(title = "Бухгалтерия", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
+    BgsGroup.create(title = "Ночной колл-центр", comment = "", cgr = 0, pids = None, opids = None, cgrMode = 0)
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Справочники -> Адреса
+  //
+  def addresses(): Unit = {
+    val countryId = AddressCountry.create(title = "РФ").id
+    val cityId = AddressCity.create(countryId = countryId, title = "г. Звенигород").id
+    val streetId = AddressStreet.create(cityid = cityId, title = "ул. Мира", pIndex = "143180").id
+    val houseId = AddressHouse.create(streetid = streetId, house = 1, frac = Some("Б"), amount = 128, comment = None, areaid = 0, quarterid = 0, boxIndex = None, dt = None, podDiapazon = "", pod = "").id
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Справочники -> Другие -> Типы платежей
+  //
+  def contractPaymentTypes(): Unit = {
+    ContractPaymentTypes.create(title = "Наличные", up = 0, `type` = 0, flag = 0)
+    ContractPaymentTypes.create(title = "Банковская карта (офлайн)", up = 0, `type` = 0, flag = 0)
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Справочники -> Другие -> Договоры - параметры
+  //
+  def contractParametersPrefs(): Unit = {
+    // Общее
+    //
+    /*  1 */ ContractParametersPref.create(pt = 1, title = "Логин", sort = 1, script = "", flags = 1, lm = now)
+    /*  2 */ ContractParametersPref.create(pt = 1, title = "Лицевой счёт", sort = 1, script = "", flags = 1, lm = now)
+    /*  3 */ ContractParametersPref.create(pt = 2, title = "Адрес подключения", sort = 1, script = "", flags = 1, lm = now)
+    /*  4 */ ContractParametersPref.create(pt = 9, title = "Телефон(ы) для связи", sort = 1, script = "", flags = 1, lm = now)
+    /*  5 */ ContractParametersPref.create(pt = 3, title = "Email", sort = 1, script = "", flags = 1, lm = now)
+
+    // Физ. лица
+    //
+    /*  6 */ ContractParametersPref.create(pt = 1, title = "Адрес регистрации", sort = 1, script = "", flags = 1, lm = now)
+    /*  7 */ ContractParametersPref.create(pt = 1, title = "Данные паспорта", sort = 1, script = "", flags = 1, lm = now)
+    /*  8 */ ContractParametersPref.create(pt = 6, title = "Дата рождения", sort = 1, script = "", flags = 1, lm = now)
+    /*  9 */ ContractParametersPref.create(pt = 1, title = "Место рождения", sort = 1, script = "", flags = 1, lm = now)
+
+    // Юр. лица
+    //
+    /* 10 */ ContractParametersPref.create(pt = 1, title = "Адрес юридический", sort = 1, script = "", flags = 1, lm = now)
+    /* 11 */ ContractParametersPref.create(pt = 1, title = "ИНН", sort = 1, script = "", flags = 1, lm = now)
+    /* 12 */ ContractParametersPref.create(pt = 7, title = "Организационно-правовая форма", sort = 1, script = "", flags = 1, lm = now)
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Справочники -> Другие -> Договоры - группы параметров
+  //
+  def contractParameterGroups(): Unit = {
+    ContractParameterGroupName.create("Физ. лицо")
+    ContractParameterGroup.create(gid = 1, pid = 1)
+    ContractParameterGroup.create(gid = 1, pid = 2)
+    ContractParameterGroup.create(gid = 1, pid = 3)
+    ContractParameterGroup.create(gid = 1, pid = 4)
+    ContractParameterGroup.create(gid = 1, pid = 5)
+    ContractParameterGroup.create(gid = 1, pid = 6)
+    ContractParameterGroup.create(gid = 1, pid = 7)
+    ContractParameterGroup.create(gid = 1, pid = 8)
+    ContractParameterGroup.create(gid = 1, pid = 9)
+    ContractParameterGroupName.create("Юр. лицо")
+    ContractParameterGroup.create(gid = 2, pid = 1)
+    ContractParameterGroup.create(gid = 1, pid = 2)
+    ContractParameterGroup.create(gid = 1, pid = 3)
+    ContractParameterGroup.create(gid = 1, pid = 4)
+    ContractParameterGroup.create(gid = 1, pid = 5)
+    ContractParameterGroup.create(gid = 2, pid = 10)
+    ContractParameterGroup.create(gid = 2, pid = 11)
+    ContractParameterGroup.create(gid = 2, pid = 12)
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Справочники -> Другие -> Договоры - значения списков -> Значения списков
+  //
+  def сontractParameterType7Values(): Unit = {
+    ContractParameterType7Values.create(pid = 12, title = "ООО")
+    ContractParameterType7Values.create(pid = 12, title = "ЗАО")
+    ContractParameterType7Values.create(pid = 12, title = "ПАО")
+  }
+
+  //--------------------------------------------------------------------------------------------------------------------
+  // Справочники -> Атрибуты -> Атрибуты
+  // Справочники -> Атрибуты -> Сущности
+  //
+  def entitySpecs(): Unit = {
+    EntitySpecAttr.create(title = "Адрес", `type` = 8, comment = "")
+    EntitySpecAttr.create(title = "S/N", `type` = 1, comment = "")
+    EntitySpec.create(title = "Коммутатор", entityspectypeid = 0, comment = "", hidden = 0, entitytitlemacros = "")
+    EntitySpecAttrLink.create(entityspecid = 1, entityspecattrid = 1, pos = 0)
+    EntitySpecAttrLink.create(entityspecid = 1, entityspecattrid = 2, pos = 0)
   }
 
 }
