c820a965b62c9a32372795fbc267ed3e7f584c4e
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-05-31 05:21:42 +0300

    Make GraalVM Polyglot demos more difficult
    JavaScript, Python.

--- bgbilling/container/polyglot/demo/ServerVersionInfo.js
+++ bgbilling/container/polyglot/demo/ServerVersionInfo.js
@@ -3,11 +3,31 @@
   Run this demo with:
 
       docker exec --tty bgbilling-billing \
-        polyglot --jvm --jvm.cp=/bgbilling/lib/app/kernel.jar \
+        node --jvm --jvm.cp=/bgbilling/lib/app/kernel.jar:/bgbilling/lib/ext/joda-time-2.9.9.jar \
           /bgbilling/polyglot/demo/ServerVersionInfo.js
 
- */
+*/
 
-const VersionInfo = Java.type('bitel.billing.common.VersionInfo');
-const versionInfoString = VersionInfo.getVersionInfo('server').getVersionString();
+// Java: Calling static method
+//
+const VersionInfoType = Java.type('bitel.billing.common.VersionInfo');
+const versionInfoString = VersionInfoType.getVersionInfo('server').getVersionString();
 console.log(versionInfoString);
+
+// Java: Using objects
+//
+const DateTimeFormatType = Java.type('org.joda.time.format.DateTimeFormat');
+const formatter = DateTimeFormatType.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z");
+const DateTimeType = Java.type('org.joda.time.DateTime');
+const dt = new DateTimeType(2018, 5, 31, 4, 0, 0, 0);
+console.log(dt.toString(formatter));
+
+// Plain old JavaScript
+//
+const fs = require('fs');
+const path = '/bgbilling/polyglot/demo';
+fs.readdir(path, (err, files) => {
+    files.forEach(file => {
+        console.log(file);
+    });
+});
--- bgbilling/container/polyglot/demo/ServerVersionInfo.py
+++ bgbilling/container/polyglot/demo/ServerVersionInfo.py
@@ -1,15 +1,34 @@
-'''
+"""
 
   Run this demo with:
 
       docker exec --tty bgbilling-billing \
-        polyglot --jvm --jvm.cp=/bgbilling/lib/app/kernel.jar --language python \
+        polyglot --jvm --jvm.cp=/bgbilling/lib/app/kernel.jar:/bgbilling/lib/ext/joda-time-2.9.9.jar --language python \
           /bgbilling/polyglot/demo/ServerVersionInfo.py
 
-'''
+"""
 
 import java
+from os import listdir
+from os.path import isfile, join
 
-version_info = java.type("bitel.billing.common.VersionInfo")
-version_info_string = version_info.getVersionInfo("server").getVersionString()
+# Java: Calling static method
+#
+version_info_type = java.type("bitel.billing.common.VersionInfo")
+version_info_string = version_info_type.getVersionInfo("server").getVersionString()
 print(version_info_string)
+
+# Java: Using objects
+#
+date_time_format_type = java.type("org.joda.time.format.DateTimeFormat")
+formatter = date_time_format_type.forPattern("yyyy-MM-dd'T'HH:mm:ss'Z")
+date_time_type = java.type("org.joda.time.DateTime")
+dt = date_time_type(2018, 5, 31, 4, 0, 0, 0)
+print(dt.toString(formatter))
+
+# Plain old Python
+#
+path = "/bgbilling/polyglot/demo"
+only_files = [f for f in listdir(path) if isfile(join(path, f))]
+for f in only_files:
+    print(f)
