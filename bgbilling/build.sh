b1102b21c947ee63aa8264cf0272b0fc24bd6c4a
root <alexanderfefelov@yandex.ru>
2018-06-01 07:38:47 +0300

    Fix "ADD failed: Forbidden path outside the build context"

--- bgbilling/build.sh
+++ bgbilling/build.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+cp --recursive ../dyn/container/* container/
 docker build --tag alexanderfefelov/bgbilling-billing .
+rm --force --recursive  \
+  container/dyn \
+  container/lib \
+  container/polyglot
