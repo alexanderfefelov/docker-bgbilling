765af4a0f427a2c4202f41dac83f7c44cde6ec93
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-06-01 15:39:45 +0300

    Fix "cannot find symbol"

--- dyn/container/dyn/com/github/alexanderfefelov/bgbilling/dyn/device/qinq/QinqProtocolHandler.java
+++ dyn/container/dyn/com/github/alexanderfefelov/bgbilling/dyn/device/qinq/QinqProtocolHandler.java
@@ -38,7 +38,7 @@ public class QinqProtocolHandler implements ProtocolHandler,
         }
         byte[] agentRemoteId = matcher.group(1).getBytes();
         byte[] vlanId = ByteBuffer.allocate(2).putShort(Short.parseShort(matcher.group(2))).array();
-        request.setOption(InetDhcpProcessor.AGENT_REMOTE_ID, agentCircuitId);
+        request.setOption(InetDhcpProcessor.AGENT_REMOTE_ID, agentRemoteId);
         request.setOption(InetDhcpProcessor.VLAN_ID, vlanId);
         logger().trace("preprocessDhcpRequest: [" + device.getId() + "] " + device.toString() + ", " + removeNewLines(request.toString()));
     }
