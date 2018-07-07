e8abcbec5cf8490ce28a4b1aa3f1cfebbacb9db5
root <alexanderfefelov@yandex.ru>
2018-07-07 07:25:16 +0300

    Add remove-all-images.sh

---
+++ utils/remove-all-images.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+read -p "All images will be removed. Are you sure? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+    docker rmi alexanderfefelov/mysql-replication
+    docker rmi alexanderfefelov/graalvm-polyglot
+    docker rmi alexanderfefelov/populator
+    docker rmi alexanderfefelov/bgbilling-mysql
+    docker rmi alexanderfefelov/bgbilling-scheduler
+    docker rmi alexanderfefelov/bgbilling-billing
+    docker rmi alexanderfefelov/bgbilling-base
+    docker rmi alexanderfefelov/bgbilling-accounting
+    docker rmi alexanderfefelov/bgbilling-access
+fi
