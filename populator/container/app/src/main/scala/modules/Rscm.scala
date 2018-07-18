27fb0d220336e10b8c935ef78310428cde7ad227
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-07-19 02:52:20 +0300

    Work on populator

--- populator/container/app/src/main/scala/modules/Rscm.scala
+++ populator/container/app/src/main/scala/modules/Rscm.scala
@@ -1,6 +1,6 @@
 package modules
 
-import com.github.alexanderfefelov.bgbilling.api.db.repository.{ScheduledTasks, Service => DbService}
+import com.github.alexanderfefelov.bgbilling.api.db.repository.{RscmService3, ScheduledTasks, Service => DbService}
 import com.github.alexanderfefelov.bgbilling.api.soap.kernel.ModuleService
 import org.joda.time.DateTime
 
@@ -11,22 +11,29 @@ object Rscm {
 
   //--------------------------------------------------------------------------------------------------------------------
   // Модули -> Редактор модулей и услуг
+  // Модули -> Товары и разовые услуги
   //
   def moduleAndServices(moduleService: ModuleService): Int = {
     val moduleIdFuture = moduleService.moduleAdd(Some("rscm"), Some("Товары и разовые услуги"))
     val moduleId = Await.result(moduleIdFuture, 10.minutes)
     /* 4 */ DbService.create("Подключение", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
       comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 10000)
+    RscmService3.create(Some("4"), Some("шт."))
     /* 5 */ DbService.create("Настройка бытового Wi-Fi-маршрутизатора", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
       comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 10000)
+    RscmService3.create(Some("5"), Some("шт."))
     /* 6 */ DbService.create("Выезд технического специалиста", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
       comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 10000)
+    RscmService3.create(Some("6"), Some("шт."))
     /* 7 */ DbService.create("Маршрутизатор MikroTik hAP ac2 (RBD52G-5HacD2HnD-TC)", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
       comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 10000)
+    RscmService3.create(Some("7"), Some("шт."))
     /* 8 */ DbService.create("Маршрутизатор TP-Link Archer C1200", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
       comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 10000)
+    RscmService3.create(Some("8"), Some("шт."))
     /* 9 */ DbService.create("Маршрутизатор Asus RT-AC53", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
       comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 10000)
+    RscmService3.create(Some("89"), Some("шт."))
     moduleId
   }
 
