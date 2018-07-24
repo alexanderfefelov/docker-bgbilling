d66a3de5121757fab2961511157b8bfad0cea372
root <alexanderfefelov@yandex.ru>
2018-07-24 12:18:21 +0300

    Monitor JMX metrix with Graphite

--- bgbilling-base/build.sh
+++ bgbilling-base/build.sh
@@ -2,5 +2,6 @@
 
 mkdir container
 cp --recursive ../dyn/container/* container/
+cp --recursive ../jmx/container/* container/
 docker build --tag alexanderfefelov/bgbilling-base .
 rm --force --recursive container
