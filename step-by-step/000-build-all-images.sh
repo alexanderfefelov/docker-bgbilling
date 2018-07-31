56535d568cde695b7d60faf43c3f940bdd52d507
root <alexanderfefelov@yandex.ru>
2018-07-31 07:38:01 +0300

    Refactor Dockerfiles

--- step-by-step/000-build-all-images.sh
+++ step-by-step/000-build-all-images.sh
@@ -4,12 +4,13 @@ function build {
     (cd ../$1 && ./build.sh)
 }
 
-build mysql \
+build bg-constants \
 && build bgbilling-base \
 && build bgbilling \
 && build bgscheduler \
 && build bginetaccess \
 && build bginetaccounting \
+&& build mysql \
 && build grafana \
 && build populator \
 && build telegraf
