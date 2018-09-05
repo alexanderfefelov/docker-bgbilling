68ff9ddf1b1019f6e70052507995088fa1a7fc07
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-09-05 19:53:41 +0300

    Fix timeout

--- populator/container/app/src/main/scala/modules/Inet.scala
+++ populator/container/app/src/main/scala/modules/Inet.scala
@@ -26,7 +26,7 @@ object Inet {
     import com.github.alexanderfefelov.bgbilling.api.db.repository.{Service => DbService}
 
     val moduleIdFuture = moduleService.moduleAdd(Some("inet"), Some("Интернет"))
-    val moduleId = Await.result(moduleIdFuture, 10.minutes)
+    val moduleId = Await.result(moduleIdFuture, 30.minutes)
     /* 1 */ DbService.create("Доступ в интернет", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
       comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 30000)
     /* 2 */DbService.create("Канал L2", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
