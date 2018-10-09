5bc5d884626d998dfb84c940f4dcd5b626ebb1eb
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-10-09 03:45:41 +0300

    Upgrade GraalVM to 1.0.0-rc7

--- bgscheduler/container/setenv.sh
+++ bgscheduler/container/setenv.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-export JAVA_HOME=/graalvm-ce-1.0.0-rc6
+export JAVA_HOME=/graalvm-ce-1.0.0-rc7
 export BGBILLING_SERVER_DIR=$BGBILLING_HOME
--- populator/container/app/src/main/scala/modules/Inet.scala
+++ populator/container/app/src/main/scala/modules/Inet.scala
@@ -30,7 +30,7 @@ object Inet {
     val moduleId = Await.result(moduleIdFuture, 30.minutes)
     /* 1 */ DbService.create("Доступ в интернет", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
       comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 30000)
-    /* 2 */DbService.create("Канал L2", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
+    /* 2 */ DbService.create("Канал L2", mid = moduleId, parentid = 0, datefrom = None, dateto = None,
       comment = "", description = "", lm = DateTime.now, isusing = Some(true), unit = 10000)
     moduleId
   }
