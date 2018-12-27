4b2515a3b644003e8cbd345d8be7731fc85097cf
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-12-27 07:15:04 +0300

    Work in progress

--- dyn/container/dyn/SysInfo.java
+++ dyn/container/dyn/SysInfo.java
@@ -1,59 +1,67 @@
-import java.util.*;
 import bitel.billing.common.VersionInfo;
 import ru.bitel.bgbilling.kernel.module.common.bean.BGModule;
 import ru.bitel.bgbilling.kernel.module.server.ModuleCache;
+import ru.bitel.bgbilling.kernel.script.server.dev.GlobalScriptBase;
+import ru.bitel.bgbilling.server.util.Setup;
+import ru.bitel.common.sql.ConnectionSet;
 
-public class SysInfo {
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.util.*;
 
-    public static void main(String[] args) {
+public class SysInfo extends GlobalScriptBase {
+
+    @Override
+    public void execute(Setup setup, ConnectionSet connectionSet) throws Exception {
         System.out.println(new Date().toString() + NL);
 
-        modules();
-        runtime();
-        systemProperties();
-        environment();
+        inspectModules();
+        inspectRuntime();
+        inspectSystemProperties();
+        inspectEnvironment();
+        inspectConnectionSet(connectionSet);
     }
 
-    private static void modules() {
+    private void inspectModules() {
         System.out.println(String.join(NL,
-            "Modules",
-            HR
+                "Modules",
+                HR
         ));
         VersionInfo kernelVer = VersionInfo.getVersionInfo("server");
         System.out.println(String.join(SPACE,
-            "0",
-            kernelVer.getModuleName(),
-            kernelVer.getVersionString()
+                "0",
+                kernelVer.getModuleName(),
+                kernelVer.getVersionString()
         ));
         List<BGModule> modules = ModuleCache.getInstance().getModulesList();
         for (BGModule module : modules) {
             VersionInfo ver = VersionInfo.getVersionInfo(module.getName());
             System.out.println(String.join(SPACE,
-                String.valueOf(module.getId()),
-                ver.getModuleName(),
-                ver.getVersionString()
+                    String.valueOf(module.getId()),
+                    ver.getModuleName(),
+                    ver.getVersionString()
             ));
         }
         System.out.println();
     }
 
-    private static void runtime() {
+    private void inspectRuntime() {
         System.out.println(String.join(NL,
-            "Runtime",
-            HR,
-            "Available processors: " + Runtime.getRuntime().availableProcessors(),
-            "Memory free / max / total, MB: "
-                + Runtime.getRuntime().freeMemory()/MB + " / "
-                + Runtime.getRuntime().maxMemory()/MB + " / "
-                + Runtime.getRuntime().totalMemory()/MB
+                "Runtime",
+                HR,
+                "Available processors: " + Runtime.getRuntime().availableProcessors(),
+                "Memory free / max / total, MB: "
+                        + Runtime.getRuntime().freeMemory() / MB + " / "
+                        + Runtime.getRuntime().maxMemory() / MB + " / "
+                        + Runtime.getRuntime().totalMemory() / MB
         ));
         System.out.println();
     }
 
-    private static void systemProperties() {
+    private void inspectSystemProperties() {
         System.out.println(String.join(NL,
-            "System properties",
-            HR
+                "System properties",
+                HR
         ));
         Map<String, String> properties = new TreeMap<>();
         System.getProperties().forEach((k, v) -> properties.put((String) k, (String) v));
@@ -63,18 +71,44 @@ public class SysInfo {
         System.out.println();
     }
 
-    private static void environment() {
+    private void inspectEnvironment() {
         System.out.println(String.join(NL,
-            "Environment",
-            HR
+                "Environment",
+                HR
         ));
         Map<String, String> env = System.getenv();
-        List<String> keys = new ArrayList(env.keySet());
+        List<String> keys = new ArrayList<>(env.keySet());
         Collections.sort(keys);
         for (String key : keys) {
             System.out.println(key + "=" + env.get(key));
         }
         System.out.println();
+    }
+
+    private void inspectConnectionSet(ConnectionSet connectionSet) throws Exception  {
+        System.out.println(String.join(NL,
+                "ConnectionSet",
+                HR
+        ));
+
+        System.out.println("Master connection");
+        inspectConnectiron(connectionSet.getConnection());
+        System.out.println("Slave connection");
+        inspectConnectiron(connectionSet.getSlaveConnection());
+        System.out.println();
+    }
+
+    private void inspectConnectiron(Connection connection) throws Exception {
+        if (connection == null) {
+            System.out.println("\tNo connection found");
+            return;
+        }
+
+        DatabaseMetaData metaData = connection.getMetaData();
+        System.out.println("\tURL: " + metaData.getURL());
+        System.out.println("\tUsername: " + metaData.getUserName());
+        System.out.println("\tProduct: " + metaData.getDatabaseProductName() + " " + metaData.getDatabaseProductVersion());
+        System.out.println("\tDriver: " + metaData.getDriverName() + " " + metaData.getDriverVersion());
     }
 
     private final static String HR = "--------------------------------------------------";
