034efeb3a28d0af5dcceec94c5d068040d4e4797
Alexander Fefelov <alexanderfefelov@yandex.ru>
2018-12-28 15:04:14 +0300

    Work in progress

--- dyn/container/dyn/com/github/alexanderfefelov/bgbilling/dyn/kernel/event/murmuring/MurmuringEventHandler.java
+++ dyn/container/dyn/com/github/alexanderfefelov/bgbilling/dyn/kernel/event/murmuring/MurmuringEventHandler.java
@@ -1,14 +1,11 @@
 package com.github.alexanderfefelov.bgbilling.dyn.kernel.event.murmuring;
 
 import com.github.alexanderfefelov.bgbilling.dyn.framework.Loggable;
-import ru.bitel.bgbilling.common.BGException;
-import ru.bitel.bgbilling.kernel.container.managed.ServerContext;
 import ru.bitel.bgbilling.kernel.contract.api.common.event.*;
 import ru.bitel.bgbilling.kernel.contract.balance.server.event.*;
 import ru.bitel.bgbilling.kernel.event.Event;
 import ru.bitel.bgbilling.kernel.event.events.*;
 import ru.bitel.bgbilling.kernel.script.server.dev.EventScriptBase;
-import ru.bitel.bgbilling.kernel.tariff.option.common.service.TariffOptionService;
 import ru.bitel.bgbilling.kernel.tariff.option.server.event.*;
 import ru.bitel.bgbilling.modules.rscm.server.event.*;
 import ru.bitel.bgbilling.server.util.Setup;
@@ -18,7 +15,7 @@ import ru.bitel.common.sql.ConnectionSet;
 public class MurmuringEventHandler extends EventScriptBase implements Loggable {
 
     @Override
-    public void onEvent(Event event, Setup setup, ConnectionSet set) throws Exception {
+    public void onEvent(Event event, Setup setup, ConnectionSet connectionSet) throws Exception {
         try {
             NestedContext.push(LOG_CONTEXT);
             switch (CLAZZ.valueOf(event.getClass().getSimpleName())) {
@@ -26,81 +23,109 @@ public class MurmuringEventHandler extends EventScriptBase implements Loggable {
                 // ru.bitel.bgbilling.kernel.event.events
                 //
 
+                case ActionAfterEvent:
+                    onActionAfterEvent((ActionAfterEvent) event, setup, connectionSet);
+                    break;
+                case ActionBeforeEvent:
+                    onActionBeforeEvent((ActionBeforeEvent) event, setup, connectionSet);
+                    break;
+                case AdditionalActionEvent:
+                    onAdditionalActionEvent((AdditionalActionEvent) event, setup, connectionSet);
+                    break;
+                case BeforeServiceDeleteEvent:
+                    onBeforeServiceDeleteEvent((BeforeServiceDeleteEvent) event, setup, connectionSet);
+                    break;
                 case CalculateEvent:
-                    onCalculateEvent((CalculateEvent) event);
+                    onCalculateEvent((CalculateEvent) event, setup, connectionSet);
                     break;
                 case CancelTariffEvent:
-                    onCancelTariffEvent((CancelTariffEvent) event);
+                    onCancelTariffEvent((CancelTariffEvent) event, setup, connectionSet);
                     break;
                 case ChangeContractLimitEvent:
-                    onChangeContractLimitEvent((ChangeContractLimitEvent) event);
+                    onChangeContractLimitEvent((ChangeContractLimitEvent) event, setup, connectionSet);
                     break;
                 case ChangeTariffByTaskEvent:
-                    onChangeTariffByTaskEvent((ChangeTariffByTaskEvent) event);
+                    onChangeTariffByTaskEvent((ChangeTariffByTaskEvent) event, setup, connectionSet);
+                    break;
+                case ContractAddingSubEvent:
+                    onContractAddingSubEvent((ContractAddingSubEvent) event, setup, connectionSet);
+                    break;
+                case ContractAddObjectEvent:
+                    onContractAddObjectEvent((ContractAddObjectEvent) event, setup, connectionSet);
+                    break;
+                case ContractCreatedEvent:
+                    onContractCreatedEvent((ContractCreatedEvent) event, setup, connectionSet);
+                    break;
+                case ContractDeleteObjectEvent:
+                    onContractDeleteObjectEvent((ContractDeleteObjectEvent) event, setup, connectionSet);
                     break;
                 case ContractLimitUserLow:
-                    onContractLimitUserLow((ContractLimitUserLow) event);
+                    onContractLimitUserLow((ContractLimitUserLow) event, setup, connectionSet);
                     break;
                 case ContractTariffDeleteEvent:
-                    onContractTariffDeleteEvent((ContractTariffDeleteEvent) event);
+                    onContractTariffDeleteEvent((ContractTariffDeleteEvent) event, setup, connectionSet);
                     break;
                 case ContractTariffUpdateEvent:
-                    onContractTariffUpdateEvent((ContractTariffUpdateEvent) event);
+                    onContractTariffUpdateEvent((ContractTariffUpdateEvent) event, setup, connectionSet);
                     break;
                 case ContractWebLoginEvent:
-                    onContractWebLoginEvent((ContractWebLoginEvent) event);
+                    onContractWebLoginEvent((ContractWebLoginEvent) event, setup, connectionSet);
                     break;
                 case GetChangeTariffDatesEvent:
-                    onGetChangeTariffDatesEvent((GetChangeTariffDatesEvent) event);
+                    onGetChangeTariffDatesEvent((GetChangeTariffDatesEvent) event, setup, connectionSet);
                     break;
                 case GetContractCardsList:
-                    onGetContractCardsList((GetContractCardsList) event);
+                    onGetContractCardsList((GetContractCardsList) event, setup, connectionSet);
                     break;
                 case GetTariffListEvent:
-                    onGetTariffListEvent((GetTariffListEvent) event);
+                    onGetTariffListEvent((GetTariffListEvent) event, setup, connectionSet);
                     break;
                 case LimitChangedEvent:
-                    onLimitChangedEvent((LimitChangedEvent) event);
+                    onLimitChangedEvent((LimitChangedEvent) event, setup, connectionSet);
                     break;
                 case ServerStartEvent:
-                    onServerStartEvent((ServerStartEvent) event);
+                    onServerStartEvent((ServerStartEvent) event, setup, connectionSet);
                     break;
                 case ServiceUpdateEvent:
-                    onServiceUpdateEvent((ServiceUpdateEvent) event);
+                    onServiceUpdateEvent((ServiceUpdateEvent) event, setup, connectionSet);
                     break;
                 case TimerEvent:
-                    onTimerEvent((TimerEvent) event);
+                    onTimerEvent((TimerEvent) event, setup, connectionSet);
                     break;
                 case ValidateTextParamEvent:
-                    onValidateTextParamEvent((ValidateTextParamEvent) event);
+                    onValidateTextParamEvent((ValidateTextParamEvent) event, setup, connectionSet);
                     break;
 
                 // ru.bitel.bgbilling.kernel.contract.balance.server.event
                 //
 
+                case ChargeEvent:
+                    onChargeEvent((ChargeEvent) event, setup, connectionSet);
+                    break;
+
                 case PaymentEvent:
-                    onPaymentEvent((PaymentEvent) event);
+                    onPaymentEvent((PaymentEvent) event, setup, connectionSet);
                     break;
 
                 // ru.bitel.bgbilling.modules.rscm.server.event
                 //
 
                 case RSCMContractServiceUpdateEvent:
-                    onRSCMContractServiceUpdateEvent((RSCMContractServiceUpdateEvent) event);
+                    onRSCMContractServiceUpdateEvent((RSCMContractServiceUpdateEvent) event, setup, connectionSet);
                     break;
 
                 // ru.bitel.bgbilling.kernel.contract.api.common.event
                 //
 
                 case ContractPasswordChangedEvent:
-                    onContractPasswordChangedEvent((ContractPasswordChangedEvent) event);
+                    onContractPasswordChangedEvent((ContractPasswordChangedEvent) event, setup, connectionSet);
                     break;
 
                 // ru.bitel.bgbilling.kernel.tariff.option.server.event
                 //
 
                 case TariffOptionListAvailableEvent:
-                    onTariffOptionListAvailableEvent((TariffOptionListAvailableEvent) event);
+                    onTariffOptionListAvailableEvent((TariffOptionListAvailableEvent) event, setup, connectionSet);
                     break;
 
                 // Default
@@ -120,115 +145,151 @@ public class MurmuringEventHandler extends EventScriptBase implements Loggable {
     // ru.bitel.bgbilling.kernel.event.events
     //
 
-    private void onCalculateEvent(CalculateEvent event) {
+    private void onActionAfterEvent(ActionAfterEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onCancelTariffEvent(CancelTariffEvent event) {
+    private void onActionBeforeEvent(ActionBeforeEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onChangeContractLimitEvent(ChangeContractLimitEvent event) {
+    private void onAdditionalActionEvent(AdditionalActionEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onChangeTariffByTaskEvent(ChangeTariffByTaskEvent event) {
+    private void onBeforeServiceDeleteEvent(BeforeServiceDeleteEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onContractLimitUserLow(ContractLimitUserLow event) {
+    private void onCalculateEvent(CalculateEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onContractTariffDeleteEvent(ContractTariffDeleteEvent event) {
+    private void onCancelTariffEvent(CancelTariffEvent event, Setup setup, ConnectionSet connectionSet) {
+        logger().trace("onEvent: " + event.toString());
+    }
+
+    private void onChangeContractLimitEvent(ChangeContractLimitEvent event, Setup setup, ConnectionSet connectionSet) {
+        logger().trace("onEvent: " + event.toString());
+    }
+
+    private void onChangeTariffByTaskEvent(ChangeTariffByTaskEvent event, Setup setup, ConnectionSet connectionSet) {
+        logger().trace("onEvent: " + event.toString());
+    }
+
+    private void onContractAddingSubEvent(ContractAddingSubEvent event, Setup setup, ConnectionSet connectionSet) {
+        logger().trace("onEvent: " + event.toString());
+    }
+
+    private void onContractAddObjectEvent(ContractAddObjectEvent event, Setup setup, ConnectionSet connectionSet) {
+        logger().trace("onEvent: " + event.toString());
+    }
+
+    private void onContractCreatedEvent(ContractCreatedEvent event, Setup setup, ConnectionSet connectionSet) {
+        logger().trace("onEvent: " + event.toString());
+    }
+
+    private void onContractDeleteObjectEvent(ContractDeleteObjectEvent event, Setup setup, ConnectionSet connectionSet) {
+        logger().trace("onEvent: " + event.toString());
+    }
+
+    private void onContractLimitUserLow(ContractLimitUserLow event, Setup setup, ConnectionSet connectionSet) {
+        logger().trace("onEvent: " + event.toString());
+    }
+
+    private void onContractTariffDeleteEvent(ContractTariffDeleteEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString() + "; contractTariffId: " + event.getContractTariffId());
     }
 
-    private void onContractTariffUpdateEvent(ContractTariffUpdateEvent event) {
+    private void onContractTariffUpdateEvent(ContractTariffUpdateEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString() + "; isAddTariff: " + event.isAddTariff() + "; contractTariffId: " + event.getContractTariff().getId());
     }
 
-    private void onContractWebLoginEvent(ContractWebLoginEvent event) {
+    private void onContractWebLoginEvent(ContractWebLoginEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onGetChangeTariffDatesEvent(GetChangeTariffDatesEvent event) {
+    private void onGetChangeTariffDatesEvent(GetChangeTariffDatesEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onGetContractCardsList(GetContractCardsList event) {
+    private void onGetContractCardsList(GetContractCardsList event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onGetTariffListEvent(GetTariffListEvent event) {
+    private void onGetTariffListEvent(GetTariffListEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onLimitChangedEvent(LimitChangedEvent event) {
+    private void onLimitChangedEvent(LimitChangedEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onServerStartEvent(ServerStartEvent event) {
+    private void onServerStartEvent(ServerStartEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onServiceUpdateEvent(ServiceUpdateEvent event) {
+    private void onServiceUpdateEvent(ServiceUpdateEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onTimerEvent(TimerEvent event) {
+    private void onTimerEvent(TimerEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
-    private void onValidateTextParamEvent(ValidateTextParamEvent event) {
+    private void onValidateTextParamEvent(ValidateTextParamEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
     // ru.bitel.bgbilling.kernel.contract.balance.server.event
     //
 
-    private void onPaymentEvent(PaymentEvent event) {
+    private void onChargeEvent(ChargeEvent event, Setup setup, ConnectionSet connectionSet) {
+        logger().trace("onEvent: " + event.toString());
+    }
+
+    private void onPaymentEvent(PaymentEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
     // ru.bitel.bgbilling.modules.rscm.server.event
     //
 
-    private void onRSCMContractServiceUpdateEvent(RSCMContractServiceUpdateEvent event) {
+    private void onRSCMContractServiceUpdateEvent(RSCMContractServiceUpdateEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
     // ru.bitel.bgbilling.kernel.contract.api.common.event
     //
 
-    private void onContractPasswordChangedEvent(ContractPasswordChangedEvent event) {
+    private void onContractPasswordChangedEvent(ContractPasswordChangedEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
     // ru.bitel.bgbilling.kernel.tariff.option.server.event
     //
 
-    private void onTariffOptionListAvailableEvent(TariffOptionListAvailableEvent event) {
+    private void onTariffOptionListAvailableEvent(TariffOptionListAvailableEvent event, Setup setup, ConnectionSet connectionSet) {
         logger().trace("onEvent: " + event.toString());
     }
 
     enum CLAZZ {
         // ru.bitel.bgbilling.kernel.event.events
         // --------------------------------------
-        // ActionAfterEvent
-        // ActionBeforeEvent
+        ActionAfterEvent,
+        ActionBeforeEvent,
         // ActionEvent
-        // AdditionalActionEvent
+        AdditionalActionEvent,
         // AppsEvent
-        // BeforeServiceDeleteEvent
+        BeforeServiceDeleteEvent,
         CalculateEvent,
         CancelTariffEvent,
         ChangeContractLimitEvent,
         ChangeTariffByTaskEvent,
-        // ContractAddingSubEvent
-        // ContractAddObjectEvent
-        // ContractCreatedEvent
-        // ContractDeleteObjectEvent
+        ContractAddingSubEvent,
+        ContractAddObjectEvent,
+        ContractCreatedEvent,
+        ContractDeleteObjectEvent,
         ContractLimitUserLow,
         // ContractObjectParameterBeforeUpdateEvent
         // ContractObjectParameterUpdateEvent
@@ -262,7 +323,7 @@ public class MurmuringEventHandler extends EventScriptBase implements Loggable {
 
         // ru.bitel.bgbilling.kernel.contract.balance.server.event
         // -------------------------------------------------------
-        // ChargeEvent
+        ChargeEvent,
         // ContractBalanceChangedEvent
         // ConvergenceBalanceEvent
         // PaymentChangingEvent
