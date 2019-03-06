6a3174eb7a1038bad1dc05e7e3fa0856c64f3cf4
Alexander Fefelov <alexanderfefelov@yandex.ru>
2019-03-06 09:47:34 +0300

    Work on populator

--- populator/container/app/src/main/scala/loaders/Addresses.scala
+++ populator/container/app/src/main/scala/loaders/Addresses.scala
@@ -20,12 +20,12 @@ object Addresses {
       case Right(data) =>
         for (city <- data.cities) {
           sql"insert into address_city (id, country_id, title) values (${city.id}, ${city.countryId}, ${city.name})".update.apply()
-          city.kladrIdOption.map(x => AddressConfig.create(tableId = "city", recordId = city.id, key = "kladrId", value = x))
-          city.fiasIdOption.map(x => AddressConfig.create(tableId = "city", recordId = city.id, key = "fiasId", value = x))
-          city.geoLatOption.map(x => AddressConfig.create(tableId = "city", recordId = city.id, key = "geoLat", value = x))
-          city.geoLonOption.map(x => AddressConfig.create(tableId = "city", recordId = city.id, key = "geoLon", value = x))
-          city.regionOption.map(x => AddressConfig.create(tableId = "city", recordId = city.id, key = "region", value = x))
-          city.areaOption.map(x => AddressConfig.create(tableId = "city", recordId = city.id, key = "area", value = x))
+          city.kladrIdOption.map(x => AddressConfig.create(tableId = "address_city", recordId = city.id, key = "kladrId", value = x))
+          city.fiasIdOption.map(x => AddressConfig.create(tableId = "address_city", recordId = city.id, key = "fiasId", value = x))
+          city.geoLatOption.map(x => AddressConfig.create(tableId = "address_city", recordId = city.id, key = "geoLat", value = x))
+          city.geoLonOption.map(x => AddressConfig.create(tableId = "address_city", recordId = city.id, key = "geoLon", value = x))
+          city.regionOption.map(x => AddressConfig.create(tableId = "address_city", recordId = city.id, key = "region", value = x))
+          city.areaOption.map(x => AddressConfig.create(tableId = "address_city", recordId = city.id, key = "area", value = x))
         }
       case Left(error) =>
         throw new RuntimeException(error)
@@ -34,8 +34,8 @@ object Addresses {
       case Right(data) =>
         for (street <- data.streets) {
           sql"""insert into address_street (id, cityid, title, p_index) values (${street.id}, ${street.cityId}, ${street.name}, "")""".update.apply()
-          street.kladrIdOption.map(x => AddressConfig.create(tableId = "street", recordId = street.id, key = "kladrId", value = x))
-          street.fiasIdOption.map(x => AddressConfig.create(tableId = "street", recordId = street.id, key = "fiasId", value = x))
+          street.kladrIdOption.map(x => AddressConfig.create(tableId = "address_street", recordId = street.id, key = "kladrId", value = x))
+          street.fiasIdOption.map(x => AddressConfig.create(tableId = "address_street", recordId = street.id, key = "fiasId", value = x))
         }
       case Left(error) =>
         throw new RuntimeException(error)
@@ -47,11 +47,11 @@ object Addresses {
           building.number match {
             case r(y, z) => (y, z)
               sql"""insert into address_house (id, streetid, house, frac, amount, areaid, quarterid, box_index, pod_diapazon, pod) values (${building.id}, ${building.streetId}, ${toInt(y)}, $z, 0, 0, 0, ${building.postalCodeOption}, "", "")""".update.apply()
-              building.kladrIdOption.map(x => AddressConfig.create(tableId = "house", recordId = building.id, key = "kladrId", value = x))
-              building.fiasIdOption.map(x => AddressConfig.create(tableId = "house", recordId = building.id, key = "fiasId", value = x))
-              building.geoLatOption.map(x => AddressConfig.create(tableId = "house", recordId = building.id, key = "geoLat", value = x))
-              building.geoLonOption.map(x => AddressConfig.create(tableId = "house", recordId = building.id, key = "geoLons", value = x))
-              building.captureDateOption.map(x => AddressConfig.create("house", building.id, "captureDate", x.toString("dd.MM.yyyy")))
+              building.kladrIdOption.map(x => AddressConfig.create(tableId = "address_house", recordId = building.id, key = "kladrId", value = x))
+              building.fiasIdOption.map(x => AddressConfig.create(tableId = "address_house", recordId = building.id, key = "fiasId", value = x))
+              building.geoLatOption.map(x => AddressConfig.create(tableId = "address_house", recordId = building.id, key = "geoLat", value = x))
+              building.geoLonOption.map(x => AddressConfig.create(tableId = "address_house", recordId = building.id, key = "geoLon", value = x))
+              building.captureDateOption.map(x => AddressConfig.create("address_house", building.id, "captureDate", x.toString("dd.MM.yyyy")))
           }
         }
       case Left(error) =>
