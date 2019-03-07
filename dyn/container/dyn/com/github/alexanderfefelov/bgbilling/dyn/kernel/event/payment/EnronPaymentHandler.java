5c868d3e72a12dde17d271b551d6a1bedbd7a09a
Alexander Fefelov <alexanderfefelov@yandex.ru>
2019-03-07 06:19:41 +0300

    Work in progress

--- dyn/container/dyn/com/github/alexanderfefelov/bgbilling/dyn/kernel/event/payment/EnronPaymentHandler.java
+++ dyn/container/dyn/com/github/alexanderfefelov/bgbilling/dyn/kernel/event/payment/EnronPaymentHandler.java
@@ -1,6 +1,14 @@
 package com.github.alexanderfefelov.bgbilling.dyn.kernel.event.payment;
 
 import com.github.alexanderfefelov.bgbilling.dyn.framework.Loggable;
+import org.apache.http.HttpHeaders;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
 import org.json.JSONObject;
 import ru.bitel.bgbilling.kernel.contract.balance.common.bean.Payment;
 import ru.bitel.bgbilling.kernel.contract.balance.server.event.PaymentEvent;
@@ -9,6 +17,20 @@ import ru.bitel.bgbilling.server.util.Setup;
 import ru.bitel.common.sql.ConnectionSet;
 
 public class EnronPaymentHandler extends EventScriptBase<PaymentEvent> implements Loggable {
+
+    public EnronPaymentHandler() {
+        PoolingHttpClientConnectionManager connectionManager = new PoolingHttpClientConnectionManager();
+        connectionManager.setMaxTotal(MAX_TOTAL_CONNECTIONS);
+        RequestConfig requestConfig = RequestConfig.custom()
+                .setConnectTimeout(CONNECT_TIMEOUT)
+                .setSocketTimeout(SOCKET_TIMEOUT)
+                .setConnectionRequestTimeout(CONNECTION_REQUEST_TIMEOUT)
+                .build();
+        httpClient = HttpClientBuilder.create()
+                .setConnectionManager(connectionManager)
+                .setDefaultRequestConfig(requestConfig)
+                .build();
+    }
 
     @Override
     public void onEvent(PaymentEvent event, Setup setup, ConnectionSet connectionSet) throws Exception {
@@ -26,10 +48,27 @@ public class EnronPaymentHandler extends EventScriptBase<PaymentEvent> implement
                 jsonObject.put("description", payment.getComment());
                 jsonObject.put("amount", payment.getSum());
                 logger().info("Handling payment: " + jsonObject);
+
+                HttpPost request = new HttpPost("http://enron.bgbilling.local:9000/handle-payment");
+                request.addHeader(HttpHeaders.CONTENT_TYPE, "Content-type: application/json; charset=utf-8");
+                request.setEntity(new StringEntity(jsonObject.toString()));
+                HttpResponse response = httpClient.execute(request);
+                logger().info("Received response: " + response.getStatusLine());
             }
         } catch (Exception e) {
             logger().error(e.getMessage());
         }
     }
+
+    private CloseableHttpClient httpClient;
+
+    private static final int MAX_TOTAL_CONNECTIONS = 150;
+
+    // Time to establish the connection with the remote host
+    private static final int CONNECT_TIMEOUT = 3000;
+    // Time waiting for data after the connection was established
+    private static final int SOCKET_TIMEOUT = 3000;
+    // Time to wait for a connection from the connection manager/pool
+    private static final int CONNECTION_REQUEST_TIMEOUT = 3000;
 
 }
