2d4db33c56429c5b75d974bdb5a436ee42514b7b
Alexander Fefelov <alexanderfefelov@yandex.ru>
2019-03-14 10:52:48 +0300

    Configure Vault

--- vault/run.sh
+++ vault/run.sh
@@ -11,7 +11,9 @@ run() {
       --volume /etc/localtime:/etc/localtime:ro --volume /etc/timezone:/etc/timezone:ro \
       --publish 8200:8200 \
       --volume $CONTAINER_NAME-file:/vault/file \
-      --volume $CONTAINER_NAME-log:/vault/log \
+      --volume $CONTAINER_NAME-logs:/vault/logs \
+      --env 'VAULT_DEV_ROOT_TOKEN_ID=root-token' \
+      --env 'VAULT_LOCAL_CONFIG={"backend": {"file": {"path": "/vault/file"}}, "default_lease_ttl": "168h", "max_lease_ttl": "720h"}' \
       --cap-add=IPC_LOCK \
       vault:1.0.3 \
     && docker run --rm --link $CONTAINER_NAME:foobar martin/wait -t 600
