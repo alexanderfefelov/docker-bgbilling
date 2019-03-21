244eff4a95421e7c2deae1a7a11a2c2080e106fa
Alexander Fefelov <alexanderfefelov@yandex.ru>
2019-03-21 13:34:42 +0300

    Haste makes waste

--- activemq/build.sh
+++ activemq/build.sh
@@ -3,7 +3,7 @@
 [ -z "$BROKER_NAME" ] && echo BROKER_NAME is required && exit 1
 
 cat activemq.template.xml \
-  | sed 's@BROKER__NAME@'"$BROKER_NAME"'@' \
-  > container/opt/apache-activemq-5.15.6/activemq.xml
+  | sed 's@BROKER_NAME@'"$BROKER_NAME"'@' \
+  > container/opt/apache-activemq-5.15.6/conf/activemq.xml
 
 docker build --tag alexanderfefelov/bgbilling-activemq-$BROKER_NAME .
